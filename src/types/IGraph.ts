d3e827cc0092edf995083442de0850d3d50d1aa7
s0561762 <s0561762@htw-berlin.de>
2019-08-19 14:12:23 +0200

    updating bayesjs dependencies

--- src/types/IGraph.ts
+++ src/types/IGraph.ts
@@ -1,15 +1,15 @@
 import { IEdge } from './index';
 
 export interface IGraph {
-  addNodeId(nodeId: string),
-  removeNodeId(nodeId: string),
+  addNodeId(nodeId: string): number,
+  removeNodeId(nodeId: string): void,
   getNodesId(): string[],
   getEdges(): IEdge[],
   containsNodeId(nodeId: string): boolean,
-  addEdge(nodeA: string, nodeB: string),
-  removeEdge(nodeA: string, nodeB: string),
+  addEdge(nodeA: string, nodeB: string) : number,
+  removeEdge(nodeA: string, nodeB: string) :void,
   areConnected(nodeA: string, nodeB: string): boolean,
   getNeighborsOf(nodeId: string): string[],
   clone(): IGraph,
-  print()
+  print():void
 }
